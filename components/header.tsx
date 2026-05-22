a723c1ce5c8cc778455ca45576be0dbe1cd4e2d3
skylinetrvl <skyline.agency.ca@gmail.com>
2026-05-22 12:41:51 -0400

    عح
    هح

--- components/header.tsx
+++ components/header.tsx
@@ -40,7 +40,7 @@ export function Header() {
       }`}
     >
       <div className="container mx-auto px-4">
-<div className="flex items-center justify-between h-24 pb-2">
+<div className="flex items-center justify-between h-24 pb-4">
           {/* Logo */}
           <Link href="/" className="flex items-center">
            <img
