3bea63a6862ad4767beb46545c3da9005a66df61
skylinetrvl <skyline.agency.ca@gmail.com>
2026-05-21 20:32:16 -0400

    عح
    عح

--- components/header.tsx
+++ components/header.tsx
@@ -46,7 +46,7 @@ export function Header() {
            <img
   src="/images/logo.png"
   alt="Skyline Travel Agency"
-  className="h-20 w-auto object-contain"
+  className="h-24 w-auto object-contain"
 />
           </Link>
 
